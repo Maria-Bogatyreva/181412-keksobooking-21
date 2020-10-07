11db7c9dda119165fa28ba90aeb8f2b57aa026fa
Maria-Bogatyreva <bogatyreva_maria@mail.ru>
2020-10-07 21:54:12 +0700

    Создание модуля pin

--- js/main.js
+++ js/main.js
@@ -1,36 +1,9 @@
 'use strict';
 (function () {
-  const PIN_HEIGHT = 70;
-  const PIN_WIDTH = 50;
+
   const AMOUNT_PINS = 8;
 
-  // Клонирование метки
-  const getPin = function (templateObject) {
-    const similarPinTemplate = document.querySelector('#pin')
-    .content
-    .querySelector('.map__pin');
 
-    const pinElement = similarPinTemplate.cloneNode(true);
-
-    pinElement.style.left = templateObject.location.x - (PIN_WIDTH / 2) + 'px';
-    pinElement.style.top = templateObject.location.y - PIN_HEIGHT + 'px';
-    pinElement.querySelector('img').src = templateObject.author.avatar;
-    pinElement.querySelector('img').alt = templateObject.offer.title;
-
-    return pinElement;
-  };
-
-  //  Функция для добавления меток на карту - Отрисовка похожих объявлений на карте
-  const addPins = function (preparedPins) {
-    const similarListPins = document.querySelector('.map__pins');
-    const fragment = document.createDocumentFragment();
-
-    preparedPins.forEach(function (element) {
-      fragment.appendChild(getPin(element));
-    });
-
-    similarListPins.appendChild(fragment);
-  };
 
   const pins = window.data.generatePins(AMOUNT_PINS); // Сгененировали массив пинов
 
@@ -80,7 +53,7 @@
     document.querySelector('.map').classList.remove('map--faded');
     document.querySelector('.ad-form').classList.remove('ad-form--disabled');
 
-    addPins(pins);
+    window.pin.addPins(pins);
 
     unblockForm(window.constant.adForm);
     unblockForm(mapFilter);
