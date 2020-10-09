c0fb915878afea07324404d794ef718000e123da
Maria-Bogatyreva <bogatyreva_maria@mail.ru>
2020-10-09 11:01:19 +0700

    Перенесла константы на верх

--- js/constant.js
+++ js/constant.js
@@ -1,12 +1,12 @@
 'use strict';
 (function () {
-  const adForm = document.querySelector('.ad-form'); // Форма добавления объявления
-  const mapPin = document.querySelector('.map__pin--main'); // Главная метка на карте
-  const mapFilter = document.querySelector('.map__filters-container'); // Фильтр на карте
   const MIN_Y = 130;
   const MAX_Y = 630;
   const MIN_X = 0;
   const MAX_X = 1200;
+  const adForm = document.querySelector('.ad-form'); // Форма добавления объявления
+  const mapPin = document.querySelector('.map__pin--main'); // Главная метка на карте
+  const mapFilter = document.querySelector('.map__filters-container'); // Фильтр на карте
 
   window.constant = {
     adForm: adForm,
