d775635b5a0bd987b12f74f3e4325285a4817a4b
Maria-Bogatyreva <bogatyreva_maria@mail.ru>
2020-10-11 19:59:03 +0700

    Открытие карточки по нажатию на метку на карте

--- js/mark.js
+++ js/mark.js
@@ -3,6 +3,9 @@
 (function () {
   const PIN_HEIGHT = 70;
   const PIN_WIDTH = 50;
+  const map = window.constant.map;
+
+  const createCard = window.card.create;
 
   // Клонирование метки
   const getMark = function (pin) {
@@ -10,17 +13,55 @@
     .content
     .querySelector('.map__pin');
 
-    const pinElement = similarPinTemplate.cloneNode(true);
+    const mark = similarPinTemplate.cloneNode(true);
 
-    pinElement.style.left = pin.location.x - (PIN_WIDTH / 2) + 'px';
-    pinElement.style.top = pin.location.y - PIN_HEIGHT + 'px';
-    pinElement.querySelector('img').src = pin.author.avatar;
-    pinElement.querySelector('img').alt = pin.offer.title;
+    mark.style.left = pin.location.x - (PIN_WIDTH / 2) + 'px';
+    mark.style.top = pin.location.y - PIN_HEIGHT + 'px';
+    mark.querySelector('img').src = pin.author.avatar;
+    mark.querySelector('img').alt = pin.offer.title;
 
-    return pinElement;
+    //  Открытие карточки
+    const openCard = function () {
+      closeCard();
+      createCard(pin);
+      const card = map.querySelector('.map__card');
+      card.querySelector('.popup__close').addEventListener('click', closeCard);
+      document.addEventListener('keydown', onCardEscPress);
+    };
+    // Закрытие карточки
+    const closeCard = function () {
+      const card = map.querySelector('.map__card');
+      if (card) {
+        map.removeChild(card);
+        mark.removeEventListener('click', onMarkClick);
+        mark.removeEventListener('keydown', onMarkEnterClick);
+        card.querySelector('.popup__close').removeEventListener('click', closeCard);
+        document.removeEventListener('keydown', onCardEscPress);
+      }
+    };
+
+    const onCardEscPress = function (evt) {
+      if (evt.key === 'Escape') {
+        evt.preventDefault();
+        closeCard();
+      }
+    };
+    const onMarkClick = function () {
+      openCard();
+    };
+    const onMarkEnterClick = function (evt) {
+      if (evt.key === 'Enter') {
+        openCard();
+      }
+    };
+
+    mark.addEventListener('click', onMarkClick);
+    mark.addEventListener('keydown', onMarkEnterClick);
+
+    return mark;
   };
 
-  //  Функция для добавления меток на карту - Отрисовка похожих объявлений на карте
+  //  Функция для добавления меток на карту
   const addMarks = function (pins) {
     const similarListPins = document.querySelector('.map__pins');
     const fragment = document.createDocumentFragment();
