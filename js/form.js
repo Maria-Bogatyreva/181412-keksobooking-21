282bf13a5cf19caec3d152323b252ba3dc0ffdaa
Maria-Bogatyreva <bogatyreva_maria@mail.ru>
2020-10-16 10:19:10 +0700

    Обработчик на отправку формы

--- js/form.js
+++ js/form.js
@@ -1,5 +1,14 @@
 'use strict';
 (function () {
+  const adForm = window.constant.adForm;
+  const deactivateMap = window.map.deactivate;
+  const save = window.backend.save;
+  const deleteMarks = window.mark.deleteMarks;
+
+//  ОТПРАВКА ДАННЫХ ФОРМЫ
+adForm.addEventListener('submit', function (evt) {
+    evt.preventDefault();
+    save(function () {}, function () {}, new FormData(adForm));
 
 
 })();
