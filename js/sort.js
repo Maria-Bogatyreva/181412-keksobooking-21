cdd1ae1169f7f525990fc7d424b5bf7b11728bb3
Maria-Bogatyreva <bogatyreva_maria@mail.ru>
2020-10-23 21:42:05 +0700

    Вынесение фильтрации по типу жилья в отдельную функцию

--- js/sort.js
+++ js/sort.js
@@ -5,16 +5,18 @@
   const mapFilter = window.constant.mapFilter;
   const housingType = mapFilter.querySelector('#housing-type');
 
+  // Функия для фильтрации меток по ТИПУ ЖИЛЬЯ
+  const filterByHousingType = function (pin) {
+    if (housingType.value === 'any') {
+      return true;
+    }
+    return pin.offer.type === housingType.value;
+  };
+
+
   //  Функция для фильтрации меток по типу жилья
   const filter = function (pins) {
-    let selectedType = housingType.value;
-
-    return pins.filter(function (pin) {
-      if (selectedType === 'any') {
-        return true;
-      }
-      return pin.offer.type === selectedType;
-    });
+    return pins.filter(filterByHousingType)
   };
 
   window.sort = {
