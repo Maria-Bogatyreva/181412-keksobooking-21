2547cab1c6520da7e442bf2b34426c6261532a82
Maria-Bogatyreva <bogatyreva_maria@mail.ru>
2020-10-30 09:58:19 +0700

    Card, debounce - замена функций на стрелочные

--- js/debounce.js
+++ js/debounce.js
@@ -2,14 +2,14 @@
 
 const DEBOUNCE_INTERVAL = 500; // ms
 
-const debounce = function (cb) {
+const debounce = (cb) => {
   var lastTimeout = null;
 
-  return function (...parameters) {
+  return (...parameters) => {
     if (lastTimeout) {
       window.clearTimeout(lastTimeout);
     }
-    lastTimeout = window.setTimeout(function () {
+    lastTimeout = window.setTimeout(() => {
       cb(...parameters);
     }, DEBOUNCE_INTERVAL);
   };
